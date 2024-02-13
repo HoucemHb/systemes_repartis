18fa1c60102b2538823df4298f884da2478b78c0
Houcem Hbiri <houcem96.hh@gmail.com>
2024-02-13 22:39:39 +0100

    TCP/UDP sockets

---
+++ UDPServer.java
@@ -0,0 +1,27 @@
+import java.net.*;
+
+public class UDPServer {
+    public static void main(String[] args) {
+        try {
+            DatagramSocket serverSocket = new DatagramSocket(9876);
+            byte[] receiveData = new byte[1024];
+
+            while (true) {
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                serverSocket.receive(receivePacket);
+                String sentence = new String(receivePacket.getData());
+                InetAddress IPAddress = receivePacket.getAddress();
+                int port = receivePacket.getPort();
+                System.out.println("Received: " + sentence);
+
+                String capitalizedSentence = sentence.toUpperCase();
+                byte[] sendData = capitalizedSentence.getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                serverSocket.send(sendPacket);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
